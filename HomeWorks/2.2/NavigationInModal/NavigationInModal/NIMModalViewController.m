f06750250b737b2a6013692e4c175b9ecc66251f
y@y2012s.com <aleeyyu@gmail.com>
2013-04-22 10:15:30 +0900

    homework 2.2

--- HomeWorks/2.2/NavigationInModal/NavigationInModal/NIMModalViewController.m
+++ HomeWorks/2.2/NavigationInModal/NavigationInModal/NIMModalViewController.m
@@ -30,11 +30,30 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
 
-    self.title = [NSString stringWithFormat:@"%d", self.navigationController.viewControllers.count];
-
     // TODO ここに navigation bar の右上をpushするボタンを配置するコードを書く
+    self.title = [NSString stringWithFormat:@"%d", self.navigationController.viewControllers.count];
+    UIBarButtonItem *rightBtn = [[UIBarButtonItem alloc] initWithTitle:@"push"
+                                                                 style:UIBarButtonItemStylePlain
+                                                                target:self
+                                                                action:@selector(pressPushBtn)];
+    self.navigationItem.rightBarButtonItem = rightBtn;
+    
     // TODO さらに、一番先頭に戻るボタンを左に配置する
+    UIBarButtonItem *leftBtn = [[UIBarButtonItem alloc] initWithTitle:@"root"
+                                                                style:UIBarButtonItemStylePlain
+                                                               target:self
+                                                               action:@selector(pressRootBtn)];
+    self.navigationItem.leftBarButtonItem = leftBtn;
+    self.navigationItem.leftItemsSupplementBackButton = YES;
+}
 
+- (void) pressPushBtn {
+    NIMModalViewController *viewController = [[NIMModalViewController alloc] init];
+    [self.navigationController pushViewController:viewController animated:YES];
+}
+
+- (void) pressRootBtn {
+    [self.navigationController popToRootViewControllerAnimated:YES];
 }
 
 - (void)didReceiveMemoryWarning
