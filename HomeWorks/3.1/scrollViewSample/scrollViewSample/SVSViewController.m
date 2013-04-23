63043038f08847cb2f2b1bde31a6042bf6806201
y@y2012s.com <aleeyyu@gmail.com>
2013-04-23 12:44:33 +0900

    homework 3.1

--- HomeWorks/3.1/scrollViewSample/scrollViewSample/SVSViewController.m
+++ HomeWorks/3.1/scrollViewSample/scrollViewSample/SVSViewController.m
@@ -8,7 +8,7 @@
 
 #import "SVSViewController.h"
 
-@interface SVSViewController ()
+@interface SVSViewController () <UIScrollViewDelegate>
 
 @end
 
@@ -18,7 +18,16 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    UIImageView *imageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"big_image.jpg"]];
+    UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:self.view.frame];
+    [scrollView addSubview:imageView];
+    scrollView.contentSize = imageView.frame.size;
+    scrollView.maximumZoomScale = 3.0;
+    scrollView.minimumZoomScale = 0.5;
+    scrollView.delegate = self;
+    [self.view addSubview:scrollView];
 
+    [self moveContent:scrollView toPoint:(CGPoint){480.0, 52.0}];
 }
 
 - (void)didReceiveMemoryWarning
@@ -27,5 +36,28 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (UIView *) viewForZoomingInScrollView:(UIScrollView *)scrollView
+{
+    UIView *view = [scrollView.subviews count] > 0 ? scrollView.subviews[0] : nil;
+    return view && [view isKindOfClass:[UIImageView class]] ? view : nil;
+}
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
+    NSLog(@"x=%.2f, y=%.2f", scrollView.contentOffset.x, scrollView.contentOffset.y);
+}
+
+- (void) moveContent:(UIScrollView *)scrollView toPoint:(CGPoint)point {
+    [UIView animateWithDuration:5.0f
+                          delay:0.0f
+                        options:UIViewAnimationOptionTransitionCurlUp
+                     animations:^{
+                         scrollView.contentOffset = point;
+                     }
+                     completion:nil];
+}
+
+- (BOOL)scrollViewShouldScrollToTop:(UIScrollView *)scrollView {
+    return YES;
+}
 
 @end
